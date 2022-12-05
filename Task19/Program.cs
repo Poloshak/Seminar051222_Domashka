9f1aa0a910929d057fd7009b4369a5cea6d36bb9
Poloshak <e.l.fomenko@yandex.ru>
2022-12-05 22:37:03 +0300

    Task23

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,7 +6,7 @@
 //12821 -> да
 
 Console.Clear();
-Console.Write("Введите пятизначное число ");
+Console.WriteLine("Введите пятизначное число ");
 int N = int.Parse(Console.ReadLine());
 int N1 = N/10000;
 int N2 = N/1000%10;
@@ -16,9 +16,9 @@ if(N>9999 && N<100000)
 {
     if(N1==N5 && N2==N4)
     {
-        Console.Write("палиндром");
+        Console.WriteLine("палиндром");
         }else{
-            Console.Write("не палиндром");
+            Console.WriteLine("не палиндром");
         }
 }else{
     Console.WriteLine("Введено не пятизначное число");
---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 4, 9. 
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите N ");
+int N = int.Parse(Console.ReadLine());
+if(N>0)
+{
+    for(int i=1; i<=N; i++)
+{
+    Console.WriteLine($"{Math.Pow(i, 3)}");
+}
+}else
+{
+    for(int i=-1; i>=N; i=i-1)
+    {
+        Console.WriteLine($"{Math.Pow(i, 3)}");
+    }
+}
+
